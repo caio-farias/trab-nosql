65d92797099c9c10292e001ed32b00f6169cf2e8
Caio Farias <caio4095@gmail.com>
2021-04-17 19:27:28 -0300

    add cassandra class; add query methods;

--- cassandra-musicapp/cassandra.js
+++ cassandra-musicapp/cassandra.js
@@ -1,14 +1,74 @@
-const {Client} = require('cassandra-driver');
-
+const { Client } = require("cassandra-driver")
+const dbName = 'musicapp'
 class Cassandra {
-  async connect(){
+
+  constructor() {
     this.client = new Client({
-      contactPoints: ['h1', 'h2'],
-      localDataCenter: 'datacenter1',
-      keyspace: 'musicapp'
+      contactPoints: ['localhost'],
+      localDataCenter: "datacenter1",
+      keyspace: dbName,
     })
   }
 
+  selectSongByTitle = async (params) => {
+    const query = "SELECT * FROM songs_by_title WHERE title=?"
+    this.client.execute(query, params, function(err, res) {
+      if(err){
+        console.log(err);
+      }else{
+        console.table(res.rows);
+      }
+    });
+  }
+
+  selectPlaylistByTags = async (params) => {
+    const query = 'SELECT * FROM playlists_by_title WHERE tags CONTAINS ? ALLOW FILTERING'
+    this.client.execute(query, params, function(err, res) {
+      if(err){
+        console.log(err);
+      }else{
+        console.table(res.rows);
+      }
+    });
+  }
+
+  selectPlaylistSongs= async (params) => {
+    const query = 'SELECT playlist_songs FROM playlists_by_title WHERE title=?';
+    this.client.execute(query, params, function(err, res) {
+      if(err){
+        console.log(err);
+      }else{
+        console.table(res.rows);
+      }
+    });
+  }
+
+  insertSong = async (params) => {
+    const query  = 'INSERT INTO songs (id, artist, album, title) VALUES (now(), ?, ?, ?)'
+    this.client.execute(query , params, function (err, res){
+      if(err){
+        console.log(err);
+      }else{
+        console.log(res);
+      }
+    })
+  }
+
+  insertBatch = async(queries) => {
+    await this.client.batch(queries, { prepare: true });
+    console.log('Data updated on cluster');
+  }
+  
+  insertPlaylist = async (params) => {
+    const query  = 'INSERT INTO playlists (id, title, playlist_songs, tags) VALUES(now(), ?, ?)'
+    this.client.execute(query , params, function (err, res){
+      if(err){
+        console.log(err);
+      }else{
+        console.table(res.table);
+      }
+    })
+  }
 }
 
-module.exports = Cassandra
+module.exports = Cassandra;
--- cassandra-musicapp/index.js
+++ cassandra-musicapp/index.js
@@ -1,43 +1,30 @@
-const cassandra = require('cassandra-driver')
+const Cassandra = require('./cassandra')
 const { generateSong, generatePlaylist} = require('./utils')
 
 async function main(exec){
-  const URI = "mongodb://localhost:27017?useUnifiedTopology=true"
-  const mongo = new Mongo(URI)
-  await mongo.connect()
-
+  const Cass = new Cassandra();
+  
   const total = 100
   const batchSize = total/10
   const it = Math.floor(total/batchSize)
-  const dbName = 'musicapp'
 
-
-
-
-
-
-  
-  for(let ii = 0; ii < it; ii++){
-    const batchSongs = []
-    const batchPlaylists = []
-
-    for(let jj = 0; jj < batchSize; jj++){
-      const song = generateSong()
-      batchSongs.push(song)
-      const playlist = generatePlaylist()
-      batchPlaylists.push(playlist)
+  const params =  [['ARTIST1', 'ALBUM1', 'SONG5'], ['ARTIST1', 'ALBUM1', 'SONG6']]
+  const insert = 'INSERT INTO songs (id, artist, album, title) VALUES (now(), ?, ?, ?)'
+  const queries = [
+    {
+      query: insert,
+      params: params[0]
+    },
+    {
+      query: insert,
+      params: params[1]
     }
+  ]
 
-    const hrstart = process.hrtime()
-    if(exec[1]){
-      await mongo.insertMany(dbName, 'songs', batchSongs)
-    }
-    if(exec[2]){
-      await mongo.insertMany(dbName, 'playlists', batchPlaylists)
-    }
-
-    console.log(`Inserted in %dms`, process.hrtime(hrstart)[1] / 1000000)
-  }
+  // Cass.insertBatch(queries)
+  Cass.selectSongByTitle(['SONG6'])
 }
+
+
 
 main([true, true, true])
